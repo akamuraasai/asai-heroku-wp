02795b14a4ebeb41e7bae936ccef4a4ff1ae3fde
Jonathan Willian <jonathan.willian.tod@gmail.com>
2019-09-14 14:32:45 -0300

    just test.

--- wp-content/themes/twentynineteen/header.php
+++ wp-content/themes/twentynineteen/header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
-    <meta name="description" content="Um site de teste muito foda" />
+    <meta name="description" content="Um site de teste muito fod*" />
 	<?php wp_head(); ?>
 </head>
 
